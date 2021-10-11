78fa7f78ed15f627d68667a268624c7b506e9b82
Joseph M <jmedina1985@hotmail.com>
2021-10-11 15:21:11 -0700

    getting started

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,5 +2,5 @@ const React = require('react');
 const Layout = require('./src/components/Layout').default
 
 exports.wrapPageElement = ({element, props}) => {
-  return <Layout>{element}</Layout>
+  return <Layout {...props}>{element}</Layout>
 }
