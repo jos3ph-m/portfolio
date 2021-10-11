901c3a7144cd85e6de41ae98c124ae3df01f0bf6
Joseph M <jmedina1985@hotmail.com>
2021-10-11 15:26:05 -0700

    getting started

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import logo from "../assets/images/logo.svg"
 import { FaAlignRight } from "react-icons/fa"
 import pageLinks from "../constants/links"
 import { Link } from "gatsby"
+
 const Navbar = () => {
   return <h2>navbar component</h2>
 }
