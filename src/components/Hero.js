c2b220e14c4a1ac4941edcabab2b78fc4a10e90b
Joseph M <jmedina1985@hotmail.com>
2021-10-13 15:20:00 -0700

    hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,7 +10,7 @@ const Hero = () => {
       <article className="hero-info">
         <div>
           <div className="underline">
-            <h1></h1>
+            <h1>I'm Joseph</h1>
           </div>
         </div>
       </article>
