061df3a107ef12677fa6619ff2c42ccd2d829040
Joseph M <jmedina1985@hotmail.com>
2021-10-14 14:59:23 -0700

    Services

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,7 +3,7 @@ import Title from "./Title"
 import services from "../constants/services"
 const Services = () => {
   return <section className="section bg-grey">
-    
+    <Title title="services" />
   </section>
 }
 
