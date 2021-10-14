5369b097034d42faa87d4701f435a9b3af066185
Joseph M <jmedina1985@hotmail.com>
2021-10-14 14:45:14 -0700

    hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -23,7 +23,7 @@ const Hero = () => {
           </div>
         </div>
       </article>
-      <StaticImage src="../assets/images/hero.svg" alt="portfolio" className="hero-img" placeholder="blurred"></StaticImage>
+      {/* <StaticImage src="../assets/images/hero.svg" alt="portfolio" className="hero-img" placeholder="blurred"></StaticImage> */}
     </section>
   </header>
 }
