69066499acee82f164b0bdff55958f85fbfa77d6
Joseph M <jmedina1985@hotmail.com>
2021-10-14 14:58:16 -0700

    Services

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,6 +3,7 @@ import React from "react"
 const Title = ({title}) => {
   return <div className="section-title">
     <h2>{title || 'default title'}</h2>
+    <div className="underline"></div>
   </div>
 }
 
