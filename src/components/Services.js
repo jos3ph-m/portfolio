2783d086bd4dcc17d2d502dead29c55a286e2fa9
Joseph M <jmedina1985@hotmail.com>
2021-10-15 15:31:12 -0700

    Services

--- src/components/Services.js
+++ src/components/Services.js
@@ -2,9 +2,12 @@ import React from "react"
 import Title from "./Title"
 import services from "../constants/services"
 const Services = () => {
-  return <section className="section bg-grey">
-    <Title title="services" />
-  </section>
+  return (
+    <section className="section bg-grey">
+      <Title title="services" />
+      <div className="section-center services-center"></div>
+    </section>
+  )
 }
 
 export default Services
