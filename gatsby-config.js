63bb98160a7d0c5e28ee7d81e54ef9e9ddcedbd3
Joseph M <jmedina1985@hotmail.com>
2021-10-16 15:17:31 -0700

    strapi source

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,7 @@ module.exports = {
       options: {
         apiURL: "http://localhost:1337",
         queryLimit: 1000, // Defaults to 100
+        collectionTypes: [],
       },
     },
     `gatsby-plugin-react-helmet`,
