dc7943af32af3e5e56dd53f5b24cde6481d7697f
Joseph M <jmedina1985@hotmail.com>
2021-10-18 13:48:43 -0700

    jobs query

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,28 +5,38 @@ import socialLinks from "../constants/social_links"
 import { StaticImage } from "gatsby-plugin-image"
 
 const Hero = () => {
-  return <header className="hero">
-    <section className="section-center hero-center">
-      <article className="hero-info">
-        <div>
-          <div className="underline">
-            <h1>I'm Joseph</h1>
-            <h4>freelance web developer</h4>
+  return (
+    <header className="hero">
+      <section className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <div className="underline"></div>
+            <h1>i'm Joseph</h1>
+            <h4>freelance web and mobile UI/UX Designer</h4>
             <Link to="/contact" className="btn">
               contact me
             </Link>
             <div className="social-links">
-              {socialLinks.map((link) => {
-                return <a href={link.url} key={link.id} className="social-link">{link.icon}</a>
+              {socialLinks.map(link => {
+                return (
+                  <a href={link.url} key={link.id} className="social-link">
+                    {link.icon}
+                  </a>
+                )
               })}
             </div>
           </div>
-        </div>
-      </article>
-      <StaticImage src="../assets/images/hero.svg" alt="portfolio" className="hero-img" placeholder="blurred"></StaticImage>
-      {/* <img src={heroImg} alt="portfolio" className="hero-img-svg"/> */}
-    </section>
-  </header>
+        </article>
+        <StaticImage
+          src="../assets/images/hero.svg"
+          alt="portfolio"
+          className="hero-img"
+          placeholder="blurred"
+        />
+        {/* <img src={heroImg} alt="portfolio" className="hero-img-svg" /> */}
+      </section>
+    </header>
+  )
 }
 
 export default Hero
