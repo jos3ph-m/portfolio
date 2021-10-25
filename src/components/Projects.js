8c2c71dbd4b2dd568285fd72b57ca56b12a9256d
Joseph M <jmedina1985@hotmail.com>
2021-10-25 12:32:11 -0700

    project component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,8 +7,8 @@ const Projects = ({ projects, title, showLink }) => {
     <section className="section projects">
       <Title title={title} />
       <div className="section-center projects-center">
-        {projects.map(project => {
-          return <Project key={project.id} />
+        {projects.map((project, index) => {
+          return <Project key={project.id} index={index} {...project} />
         })}
       </div>
       {showLink && (
