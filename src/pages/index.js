55ca324bdb7892c9c3fc91f21f2162a6f81adb16
Joseph M <jmedina1985@hotmail.com>
2021-10-24 18:45:31 -0700

    project query

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,8 @@ import Services from "../components/Services"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Seo from "../components/Seo"
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  console.log(data)
   return (
     <>
       <main>
@@ -16,5 +17,32 @@ const IndexPage = () => {
     </>
   )
 }
+
+export const query = graphql`
+  {
+    allStrapiProject(filter: { featured: { eq: true } }) {
+      nodes {
+        description
+        featured
+        github
+        id
+        slug
+        title
+        url
+        stack {
+          id
+          title
+        }
+        image {
+          localFile {
+            childImageSharp {
+              gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED)
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
