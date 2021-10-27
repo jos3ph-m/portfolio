378122eb07c8779646290a608997a839dc752ed0
Joseph M <jmedina1985@hotmail.com>
2021-10-26 18:32:50 -0700

    single project component

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,7 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { FaGithubSquare, FaShareSquare } from "react-icons/fa"
 import { Link } from "gatsby"
 
-const Project = ({ description, title, github, stack, url, image }) => {
+const Project = ({ description, title, github, stack, url, image, index }) => {
   return <h2>single project component</h2>
 }
 
