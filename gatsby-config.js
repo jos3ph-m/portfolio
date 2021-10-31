45c4c3b32aa184ebeec1330751650f5c5e0f502c
Joseph M <jmedina1985@hotmail.com>
2021-10-30 18:47:39 -0700

    About page

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         apiURL: `http://localhost:1337`,
         queryLimit: 1000, // Defaults to 100
         collectionTypes: [`job`, `project`],
-        singleTypes: [`home-page`, `contact`],
+        singleTypes: [`home-page`],
       },
     },
   ],
