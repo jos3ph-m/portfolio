44e09df1966730cb91bf86323a55bb00004ba8ea
Joseph M <jmedina1985@hotmail.com>
2021-11-01 13:57:34 -0700

    About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,7 +11,20 @@ const About = ({ data }) => {
     <>
       <section className="about-page">
         <div className="section-center about-center">
-          <img src={image[0].localFile.publicURL} alt={title} />
+          <img
+            src={image[0].localFile.publicURL}
+            alt={title}
+            className="about-img-svg"
+          />
+          <article className="about-text">
+            <Title title={title} />
+            <p>{info}</p>
+            <div className="about-stack">
+              {stack.map(item => {
+                return <span key={item.id}>{item.title}</span>
+              })}
+            </div>
+          </article>
         </div>
       </section>
     </>
