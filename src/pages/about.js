9b60ea4c20784c17ab58ca9b61a75705bf760506
Joseph M <jmedina1985@hotmail.com>
2021-11-01 13:55:45 -0700

    About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,9 @@ import Title from "../components/Title"
 import Seo from "../components/Seo"
 
 const About = ({ data }) => {
-  const { strapiAbout: { title, image, info, stack } = data 
+  const {
+    strapiAbout: { title, image, info, stack },
+  } = data
   return (
     <>
       <section className="about-page">
