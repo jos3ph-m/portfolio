33e054cfab5a2f0feba932b48ac499600f0cae94
Joseph M <jmedina1985@hotmail.com>
2021-11-02 16:35:04 -0700

    Error page and footer

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ import Seo from "../components/Seo"
 const Error = () => {
   return (
     <>
-      <main>
+      <main className="error-page">
         <div className="error-container"></div>
       </main>
     </>
