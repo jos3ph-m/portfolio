7383914c67c67658d26717fa507f192c8e90707e
Joseph M <jmedina1985@hotmail.com>
2021-11-04 16:30:26 -0700

    contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,7 @@ const contact = () => {
           <h3>get in touch</h3>
           <form>
             <div className="form-group">
-              <input type="text" />
+              <input type="text" name="name" />
             </div>
           </form>
         </article>
