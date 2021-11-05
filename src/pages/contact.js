9e0e4cd3f942f018d3d79850f8a3cc3801baa33f
Joseph M <jmedina1985@hotmail.com>
2021-11-05 16:53:07 -0700

    contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -14,6 +14,7 @@ const contact = () => {
                 placeholder="name"
                 className="form-control"
               />
+              <input />
             </div>
           </form>
         </article>
