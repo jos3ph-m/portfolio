a56a15d6e1ba72d0cfe95cb6d93640c459eae4c5
Joseph M <jmedina1985@hotmail.com>
2021-11-06 17:32:39 -0700

    sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,7 +5,11 @@ import { Link } from "gatsby"
 import { FaTimes } from "react-icons/fa"
 const Sidebar = () => {
   const isOpen = true
-  return <aside className={isOpen ? "sidebar show-sidebar"}>sidebar component</aside>
+  return (
+    <aside className={isOpen ? "sidebar show-sidebar" : "sidebar"}>
+      sidebar component
+    </aside>
+  )
 }
 
 export default Sidebar
