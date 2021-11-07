72db7464d7cfd91eb3fc935a5a0c92522f0e6949
Joseph M <jmedina1985@hotmail.com>
2021-11-06 17:31:42 -0700

    sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,6 +4,7 @@ import socialLinks from "../constants/social_links"
 import { Link } from "gatsby"
 import { FaTimes } from "react-icons/fa"
 const Sidebar = () => {
+  const isOpen = true
   return <h2>sidebar component</h2>
 }
 
