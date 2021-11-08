c2b3d5945bc4735355ff3ca7b6e86881946dd8c3
Joseph M <jmedina1985@hotmail.com>
2021-11-08 15:45:57 -0800

    Sidebar logic

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,7 +14,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Navbar toggleSidebar={toggleSidebar} />
-      <Sidebar isOpen={isOpen} toggleSidebar={} />
+      <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
       {children}
       <Footer />
     </>
