a60bf8fc3eb8ce6bf66f970a60599a7a6886bad5
Joseph M <jmedina1985@hotmail.com>
2021-11-11 16:02:58 -0800

    completing SEO

--- src/pages/projects/{StrapiProject.title}.js
+++ src/pages/projects/{StrapiProject.title}.js
@@ -5,7 +5,7 @@ import Seo from "../../components/Seo"
 const ProjectTemplate = ({ pageContext: { title }, data }) => {
   return (
     <>
-      <Seo />
+      <Seo title={} />
       <main className="project-template-page">
         <h2>{title}</h2>
         <p>{data.strapiProject.description}</p>
